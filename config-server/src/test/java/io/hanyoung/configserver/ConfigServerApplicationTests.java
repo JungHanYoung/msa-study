1adf5d9ba9a074c61d3caffac9cc03fb1644c0ff
JungHanYoung <8735132@naver.com>
2020-06-27 17:47:06 +0900

    project init

---
+++ config-server/src/test/java/io/hanyoung/configserver/ConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package io.hanyoung.configserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
